91690bad8a326c82be46e17fe259994e3e525a3f
TASTYBURGER97 <tihonov.artyom1997@yandex.ru>
2022-12-16 15:59:14 +0400

    Помогоите пожалуйста : консоль не показывает результаты последних двух заданий

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ////////////TASK1//////////
+        System.out.println("////////////TASK1//////////");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+
+    {////////////TASK2//////////
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("////////////TASK1//////////");
+
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperFullName);
+    }
+
+    { ////////////TASK3//////////
+        System.out.println("////////////TASK3//////////");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника - " + fullName.replace("ё", "е"));
     }
 }
